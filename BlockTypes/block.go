defce3b100f23ac317b989ba316d74ba4719c270
Vitalii Svinchyak <vitalii.svinchyak@gmail.com>
2018-07-27 18:56:50 +0300

    Add width format checking

--- BlockTypes/block.go
+++ BlockTypes/block.go
@@ -1,15 +1,40 @@
 package BlockTypes
 
+import (
+	"regexp"
+)
+
 type BlockProperties struct {
+	Id          string
+	Text        string
 	Width       string
 	Height      string
 	Row         int
 	Col         int
 	Border      int
 	BorderColor string
-	Children    []BlockProperties
-	Parent      *BlockProperties
+	Children    []BaseBlockI
+	Parent      BaseBlockI
 }
 
-type BaseBlock interface {
+type BaseBlockI interface {
+	SetWidth(width string)
+}
+
+type Block struct {
+	BlockProperties
+}
+
+func (b Block) SetWidth(width string) {
+	matched, err := regexp.MatchString("[0-9]+[pt%]", width)
+
+	if err != nil {
+		panic(err)
+	}
+	if !matched {
+		panic("Width " + width + " is not valid")
+	}
+
+	b.Width = width
+	// not working
 }
--- Parser/CmlParser.go
+++ Parser/CmlParser.go
@@ -4,19 +4,26 @@ import (
 	"cml/BlockTypes"
 	"strings"
 	"strconv"
+	"reflect"
 )
 
-func ParseString(cml string) []BlockTypes.BlockProperties {
-	var cmlTree []BlockTypes.BlockProperties
+func ParseString(cml string) []BlockTypes.Block {
+	var cmlTree []BlockTypes.Block
+	var knownTypes = []string{"block"}
 
 	var rows = strings.Split(string(cml), "\n")
 	var lastNesting = 0
-	var lastElement *BlockTypes.BlockProperties
+	var lastElement *BlockTypes.Block
 
-	for _, row := range rows {
+	for rowNumber, row := range rows {
 		var nesting, row = detectNesting(row)
 		var rowParameters = strings.Split(row, " ")
-		var block = parseProperties(rowParameters)
+		var blockType = rowParameters[0]
+		var knownType, _ = inArray(blockType, knownTypes)
+		if !knownType {
+			panic("Unknown block type: " + blockType + " on line " + strconv.Itoa(rowNumber))
+		}
+		var block = parseProperties(rowParameters, rowNumber)
 
 		if nesting > lastNesting {
 			block.Parent = lastElement
@@ -34,19 +41,24 @@ func ParseString(cml string) []BlockTypes.BlockProperties {
 	return cmlTree
 }
 
-func parseProperties(properties []string) BlockTypes.BlockProperties {
-	var block = BlockTypes.BlockProperties{Children: []BlockTypes.BlockProperties{}}
+func parseProperties(properties []string, rowNumber int) BlockTypes.Block {
+	var block = BlockTypes.Block{}
 
 	for _, property := range properties {
 		var propertySplitted = strings.Split(property, ":")
 		if len(propertySplitted) == 2 {
+			var propertyName = propertySplitted[0]
 			var propertyValue = propertySplitted[1]
-			switch propertySplitted[0] {
+
+			switch propertyName {
 			case "width":
-				block.Width = propertyValue
+				block.SetWidth(propertyValue)
 				break
 			case "height":
 				block.Height = propertyValue
+				break
+			case "id":
+				block.Id = propertyValue
 				break
 			case "row":
 				i, _ := strconv.Atoi(propertyValue)
@@ -56,6 +68,11 @@ func parseProperties(properties []string) BlockTypes.BlockProperties {
 				i, _ := strconv.Atoi(propertyValue)
 				block.Col = i
 				break
+			case "text":
+				block.Text = string([]rune(propertyValue)[1:len(propertyValue)])
+				break
+			default:
+				panic("Unknown property " + propertyName + " on line " + strconv.Itoa(rowNumber))
 			}
 		}
 	}
@@ -76,4 +93,24 @@ func detectNesting(row string) (int, string) {
 	}
 
 	return length / 4, string(rowRunes[length:len(row)])
+}
+
+func inArray(val interface{}, array interface{}) (exists bool, index int) {
+	exists = false
+	index = -1
+
+	switch reflect.TypeOf(array).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(array)
+
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(val, s.Index(i).Interface()) == true {
+				index = i
+				exists = true
+				return
+			}
+		}
+	}
+
+	return
 }
